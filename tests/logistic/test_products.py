c0d9ed815a6cb5cb0e59e3842c7114d8c3726d3d
berson969 <berson969@gmail.com>
2022-12-11 13:19:04 +0100

    Tests 2

--- tests/logistic/test_products.py
+++ tests/logistic/test_products.py
@@ -2,7 +2,13 @@ from rest_framework.test import APIClient, APITestCase
 
 
 class TestAPIView(APITestCase):
-    def test_sample_view(self):
+    def test_products_view(self):
         client = APIClient()
         response = client.get('/api/v1/products/')
         self.assertEqual(response.status_code, 200)
+
+    def test_stocks_view(self):
+        client = APIClient()
+        response = client.get('/api/v1/stocks/')
+        self.assertEqual(response.status_code, 200)
+
