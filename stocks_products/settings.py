b450ed2722625c725e036846d048d23651d642ce
berson969 <berson969@gmail.com>
2022-12-11 12:51:07 +0100

    Commit with tests

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -20,7 +20,7 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(BASE_DIR.parent / '.env')
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
